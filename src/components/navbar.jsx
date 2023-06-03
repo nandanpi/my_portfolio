289dbcbcc77d4e40faddf15daaa674534eeaf370
Nandan R Pai <51937330+nandanpi@users.noreply.github.com>
2023-06-03 12:34:49 +0530

    First

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
 import Switcher from "../Switcher";
 import "../index.css"
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
+    const [nav,setNav] = useState(true)
+    const handleNav = () => {
+        setNav(!nav)
+    }
     return (
         <>
-        <div className="flex dark:bg-black bg-white justify-evenly border-b-2 ">
-            <div><h1 className="text-black dark:text-white font-bold text-2xl p-3"><a href="/">nandanpi</a></h1></div>
-            <div className="justify-center">
-                <ul className="text-black dark:text-white flex text-xl font-bold space-x-6 p-4">
-                    <li><a href="/">Work</a></li>
-                    <li><a href="/timeline">Timeline</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
+        <div className="dark:bg-black bg-white  border-b-2 border-b-gray-400 dark:border-b-gray-400 ">
+            <div className="hidden md:flex justify-evenly">
+                <div><h1 className="text-black dark:text-white font-bold text-2xl p-3"><a href="/">nandanpi</a></h1></div>
+                <div className="justify-center">
+                    <ul className="text-black dark:text-white flex text-xl font-bold space-x-6 p-4">
+                        <li><a href="/">Work</a></li>
+                        <li><a href="/timeline">Timeline</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="p-4 hidden md:flex"><Switcher /></div>
             </div>
-            <div className="p-4"><Switcher /></div>
         </div>
+            <div>
+                <div className="dark:bg-black bg-white  border-b-2 border-b-gray-400 dark:border-b-gray-400 flex justify-evenly md:hidden">
+                    <div className="p-4">
+                        <div onClick={handleNav} className="md:hidden bg-white rounded-[2px] w-fit h-fit">
+                            {!nav ? <AiOutlineClose size={30}/>:<AiOutlineMenu size={30} />}
+                        </div>
+                    </div>
+                    <div><h1 className="text-black dark:text-white font-bold text-2xl  flex md:hidden p-3"><a href="/">nandanpi</a></h1></div>
+                    <div className={!nav ? ' w-full absolute mt-20 bg-white dark:bg-black duration-500 ease-in':'bg-white dark:bg-black ease-out duration-700 fixed top-[-100%]'}>
+                        <ul className=" uppercase text-black dark:text-white text-center">
+                            <li className="p-4 text-teal dark:text-blue"><a href="/">Work</a></li>
+                            <li className="p-4 text-teal dark:text-blue"><a href="/timeline">Timeline</a></li>
+                            <li className="p-4 text-teal dark:text-blue"><a href="/contact">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div className=" md:hidden flex p-4"><Switcher /></div>
+                </div>
+            </div>
+
         </>
     );
 }
--- src/components/work.jsx
+++ src/components/work.jsx
@@ -2,12 +2,16 @@ import React from "react";
 const Work = () => {
     return (
         <>
-            <div className="flex justify-center">
+            <div className="justify-center hidden md:flex">
                 <div className="flex justify-center items-center h-screen">
-
                         <h1 className=" text-black dark:text-white inline-block text-6xl type2 space-x-1">
-                            <span className="type1">Hey. I'm</span><span className=" text-orange-600 text-6xl"> Nandan Pai.</span></h1>
-
+                            <span>Hey. I'm</span><span className=" text-orange-600 text-6xl"> Nandan Pai.</span></h1>
+                </div>
+            </div>
+            <div className="justify-center flex md:hidden">
+                <div className="flex justify-center items-center h-screen">
+                    <h1 className=" text-black dark:text-white inline-block text-3xl type2 space-x-1">
+                        <span>Hey. I'm</span><span className=" text-orange-600 text-3xl"> Nandan Pai.</span></h1>
                 </div>
             </div>
         </>
