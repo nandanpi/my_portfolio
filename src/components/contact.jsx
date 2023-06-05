59290ae05979d1670e9048455b0a794b225467cc
Nandan R Pai <51937330+nandanpi@users.noreply.github.com>
2023-06-05 20:06:12 +0530

    Fixed contacts

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,17 +2,17 @@ import React from "react";
 const Contact = () => {
     return (
         <>
-        <div className="flex justify-center mt-20 mx-auto w-full md:max-w-4xl">
-            <form action="https://getform.io/f/f57e8d00-2912-4a86-ac0a-82092cb72a80" method="POST" className="flex w-fit">
+        <div className="flex justify-center mt-20 mx-auto w-7/12 md:max-w-4xl">
+            <form action="https://getform.io/f/f57e8d00-2912-4a86-ac0a-82092cb72a80" method="POST" className="flex flex-col w-full md:w-7/12">
                 <div className="space-y-3">
                     <div>
                         <p className="text-black dark:text-white text-2xl font-bold ">Contact</p>
                     </div>
                     <div>
-                        <input type="text" name="name" placeholder="Name" className="p-2 bg-transparent border-2 focus:outline-none rounded-md text-black dark:text-white"/>
+                        <input type="text" name="name" placeholder="Name" className="p-2 bg-transparent border-2 focus:outline-none rounded-md text-black dark:text-white w-full"/>
                     </div>
                     <div>
-                        <input type="email" name="email" placeholder="Email" className="p-2 bg-transparent border-2 focus:outline-none rounded-md text-black dark:text-white"/>
+                        <input type="email" name="email" placeholder="Email" className="p-2 bg-transparent border-2 focus:outline-none rounded-md text-black dark:text-white w-full"/>
                     </div>
                     <div>
                         <textarea name="message" placeholder="Your message" rows="10"
