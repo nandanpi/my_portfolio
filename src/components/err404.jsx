828319516648084699ab9bcdf06a02bf8346f756
Nandan R Pai <51937330+nandanpi@users.noreply.github.com>
2023-06-05 18:25:16 +0530

    404 reworked

--- src/components/err404.jsx
+++ src/components/err404.jsx
@@ -9,7 +9,7 @@ const Err404 = () => {
                 <img src={cat_gif} alt="Oh!no" className="rounded-xl mt-20"/>
             </div>
             <div className="text-center space-y-3 text-black dark:text-white">
-                    <h1 className="text-3xl md:text-6xl">Oops! 404</h1>
+                    <h1 className="text-3xl md:text-6xl pt-3">Oops! 404</h1>
                     <p className="text-xl md:text-4xl">Looks like you took a wrong turn, let's get you back on track shall we?</p>
                     <button className="p-3 rounded-xl bg-orange-600 text-black font-bold "><a href="/">Go to Home</a></button>
             </div>
